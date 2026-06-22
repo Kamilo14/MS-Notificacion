e80780cbce120fe9b5c04424ed0d41acf5c22935
Camilo Calderon <cam.calderono@duocuc.cl>
2026-06-21 23:01:22 -0400

    Add files via upload

---
+++ MSNotificacion/src/test/java/Catastrofe/MSNotificacion/MsNotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package Catastrofe.MSNotificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
